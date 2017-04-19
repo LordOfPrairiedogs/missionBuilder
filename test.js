09cb272a0b7792ff04a2a02c69b423701ec0a5d4
a83979 <darby.dehaai@ngc.com>
2017-04-19 17:24:14 -0500

    Merge branch 'master' of https://github.com/LordOfPrairiedogs/missionBuilder into origin/master
    # Conflicts:
    #	test.js

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 /**
  * Created by a83979 on 4/19/2017.
  */
+MG = require('C:/projects/missionBuilder/missionGenerator');
+mgData = new MG();
+groupName = 'test'.toUpperCase()
+mgData.addToGroup(groupName, ['test1', 'test2', 'test3'])
+mgData.masterDictionary
+
+groupList = mgData.masterDictionary[groupName];
