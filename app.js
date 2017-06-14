f757880116d733409da3bf4b7c7ea13d636dbf8d
LordOfPrairiedogs <lordofprairiedogs@gmail.com>
2017-06-14 17:30:37 -0500

    Removed a 400 error Now should use websockets instead of long-polling.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http = require('http');
-var io = require('socket.io');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var index = require('./routes/index');
 var users = require('./routes/users');
 var build = require('./routes/build');
@@ -15,9 +16,6 @@ var mg = new MG();
 mg.loadDictionary('../../public/files/majestic12.json');
 // mg.loadDictionary('../../public/files/spycraft.json');
 
-
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,7 +24,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,51 +34,51 @@ app.use('/build', build);
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-var listener =http.createServer(app).listen ( function () {
+var listener = http.listen(80, function () {
     console.log('Example app listening ' + listener.address().port);
 });
 io = io.listen(listener);
 
-io.sockets.on("connection",function(socket){
-  /*Associating the callback function to be executed when client visits the page and
-   websocket connection is made */
+io.sockets.on("connection", function (socket) {
+    /*Associating the callback function to be executed when client visits the page and
+     websocket connection is made */
 
     var message_to_client = {
-        data:"Connection with the server established"
+        data: "Connection with the server established"
     }
     socket.send(JSON.stringify(message_to_client));
-  /*sending data to the client , this triggers a message event at the client side */
+    /*sending data to the client , this triggers a message event at the client side */
     console.log('Socket.io Connection with the client established');
-    socket.on("message",function(data){
-      /*This event is triggered at the server side when client sends the data using socket.send() method */
+    socket.on("message", function (data) {
+        /*This event is triggered at the server side when client sends the data using socket.send() method */
         console.log("...in message");
-        data = mg.run(data,0,mg.run)
+        data = mg.run(data, 0, mg.run)
         console.log(data);
-      /*Printing the data */
+        /*Printing the data */
         var ack_to_client = {
-            data: mg.run(data,0,mg.run)
+            data: mg.run(data, 0, mg.run)
         }
         socket.send(JSON.stringify(ack_to_client));
-      /*Sending the Acknowledgement back to the client , this will trigger "message" event on the clients side*/
+        /*Sending the Acknowledgement back to the client , this will trigger "message" event on the clients side*/
     });
-    socket.on("addToGroup",function(data){
+    socket.on("addToGroup", function (data) {
         /*This event is triggered at the server side when client sends the data using socket.send() method */
         var jsonData = JSON.parse(data);
         console.log("...in addToGroup");
@@ -88,7 +86,7 @@ io.sockets.on("connection",function(socket){
         var entryList = jsonData.entryList;
         console.log("group: " + group);
         console.log("entryList: " + entryList);
-        mg.addToGroup(group,entryList,true);
+        mg.addToGroup(group, entryList, true);
 
         /*Printing the data */
         var ack_to_client = {
